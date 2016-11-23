069a3cd27f4f4e1ca8d6673e4527d9850068fa7d
fengluo <fengluo17@gmail.com>
2016-11-23 22:09:01 +0800

    feat: add basic function

---
+++ spider.py
@@ -0,0 +1,86 @@
+#coding:utf-8
+import gevent.monkey
+gevent.monkey.patch_all()
+from gevent.queue import Empty, Queue
+import gevent
+import requests
+from urlparse import urlparse, parse_qs
+from extract import extract, extract_all
+from route import route
+
+ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.83 Safari/537.1'
+
+class Spider(object):
+    cookie = None
+    headers = {}
+
+    def __init__(self, route):
+        self.queue = Queue()
+        self.route = route
+
+    def _fetch(self):
+        queue = self.queue
+        timeout = self.timeout
+        route = self.route
+        while True:
+            try:
+                url = queue.get(timeout=timeout+10)
+            except Empty:
+                return
+
+            headers = self.headers
+
+            if self.cookie:
+                headers['Cookie'] = self.cookie
+            headers['User-Agent'] = ua
+            try:
+                req = requests.get(url, timeout=timeout, headers=headers)
+                p = urlparse(req.url)
+
+                cls, args = route.match(p.path)
+                if cls:
+                    o = cls(req)
+                    r = o.get(*args)
+                    if r:
+                        for i in r:
+                            if i:
+                                queue.put(i)
+
+            except requests.exceptions.Timeout as e:
+                print 'timeout:',url
+                self.queue.put(url)
+
+
+    def run(self, num=10, timeout=60):
+        self.timeout = timeout
+        for i in xrange(num):
+            g = gevent.spawn(self._fetch)
+        g.join()
+        # gevent.shutdown()
+
+    def put(self, url):
+        self.queue.put(url)
+
+
+class Handler(object):
+
+    def __init__(self, request):
+        p = urlparse(request.url)
+        request.arguments = parse_qs(p.query, 1)
+        self.request = request
+        self.html = request.content
+
+    def get_argument(self, name, default=None):
+        result = self.request.arguments.get(name, None)
+        if result is None:
+            return default
+        return result[0].encode('utf-8', 'ignore')
+
+    def extract(self, begin, end):
+        return extract(begin, end, self.html)
+
+    def extract_all(self, begin, end):
+        return extract_all(begin, end, self.html)
+
+
+spider = Spider(route)
